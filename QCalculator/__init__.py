38f580a6e8617b49ac30fee3b9a0461598adea4c
AntonKutuzov <akutuzovsssss@gmail.com>
2025-05-06 19:15:25 +0200

    Resolved issue with Assumption being not callable

--- QCalculator/__init__.py
+++ QCalculator/__init__.py
@@ -2,3 +2,4 @@ from QCalculator._settings import SETTINGS
 from QCalculator.Datum import Datum
 from QCalculator.Formula import Formula
 from QCalculator.LinearIterator import LinearIterator
+from QCalculator.Assumption import Assumption
